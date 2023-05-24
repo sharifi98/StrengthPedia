d924da7981396323a0af2cd4fbbe69b90a83e090
Hossein Sharifi <47387238+sharifi98@users.noreply.github.com>
2023-05-24 12:49:41 +0200

    Initial Commit

---
+++ StrengthPedia/StrengthPediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrengthPediaApp.swift
+//  StrengthPedia
+//
+//  Created by Hossein Sharifi on 24/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct StrengthPediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
